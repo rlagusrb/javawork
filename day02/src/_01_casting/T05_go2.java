1c044908075774930341ce515625398e6741425a
rlagusrb <hkkim030621@naver.com>
2024-05-08 17:57:26 +0900

    StringClass

--- day02/src/_01_casting/T05_go2.java
+++ day02/src/_01_casting/T05_go2.java
@@ -31,6 +31,7 @@ public class T05_go2 {
 		
 		
 		
+		
 
 		
 		
---
+++ day02/src/_02_referenceType/T01_StringClass.java
@@ -0,0 +1,28 @@
+package _02_referenceType;
+
+import javax.xml.transform.stax.StAXSource;
+
+public class T01_StringClass {
+
+	public static void main(String[] args) {
+		String name = "홍길동";
+		String hobby = "공부하기";
+		
+		System.out.println("주소사 같은가? " + name == hobby);
+		
+		String str1 = "의료용";
+		String str2 = "의료용";
+		System.out.println("주소가 같은가?" + (str1 == str2));
+		
+		String myName = "홍길동";
+		System.out.println("주소가 같은가? " + (name == myName) );
+		
+		String str3 = new String("영상");
+		String str4 = new String("영상");
+		System.out.println("주소가 같은가? " + (str3 == str4));
+		
+		System.out.println("주소가 같은가? " + str3.equals(str4));
+
+	}
+
+}
